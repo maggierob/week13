48a6548ba7d6e9083e1b29bf470a8e344f2d8d84
maggierob <85065883+maggierob@users.noreply.github.com>
2022-01-15 19:51:07 +0100

    Add files via upload
    first commit

---
+++ calculator.js
@@ -0,0 +1,42 @@
+function showSum () {
+    let number1 = prompt ('Type the first number');
+    let number2 = prompt ('Type the second number');
+    let result = Number(number1) + Number(number2);
+    //let result = number1 + number2; 
+    alert(result); 
+}
+
+
+function showSubtraction () {
+    let number1 = prompt ('Type the first number');
+    let number2 = prompt ('Type the second number');
+
+    let result = number1 - number2; 
+    alert(result); 
+}
+
+function showDivision () {
+    let number1 = prompt ('Type the first number');
+    let number2 = prompt ('Type the second number');
+    let result = number1 / number2; 
+    alert(result);
+}
+
+function showMultiplication () {
+    let number1 = prompt ('Type the first number');
+    let number2 = prompt ('Type the second number');
+    let result = number1 * number2; 
+    alert(result);
+}
+
+//let number1 = Number(prompt('Type the first number'));
+//let number2 = Number(prompt('Type the second number'));
+
+//let result = number1 + number2;
+//alert(result);
+
+//let number1 = prompt('Type the first number');
+//let number2 = prompt('Type the second number');
+
+//let result = Number(number1) + Number(number2);
+//alert(result);
---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+function showMessage () {
+    let name = prompt('What is your name?');
+    let message_to = `Hi, ${name}`; 
+    alert(message_to);
+}
+
+
